9f41565aecfb338bafed6ddc418cd70df31a192b
Chillcraft <94008987+Chillcraft06@users.noreply.github.com>
2023-12-04 09:44:07 -0500

    commit project to repository

---
+++ ICS4UStudyProjectProject/src/ics4ustudyprojectproject/ICS4UStudyProjectProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ics4ustudyprojectproject;
+
+/**
+ *
+ * @author jochi2827
+ */
+public class ICS4UStudyProjectProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
